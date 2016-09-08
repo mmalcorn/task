ac5e485b745b98502460f4c26ab448d3793979ab
alexandre leibler and meredith alcorn <alexandre@>
2016-09-08 15:52:34 -0400

    replace in line html in app.php for the tasks route with a twig template called create_task.html.twig

--- app/app.php
+++ app/app.php
@@ -18,32 +18,16 @@
     //NOTE: Make sure that you inlcude the use ($silex_app).
     //      this is what allows us to call the render method
     $silex_app->get("/", function() use ($silex_app) {
-        //  <form action='/tasks' method='post'>
-        //      <label for='description'>Task Description</label>
-        //      <input id='description' name='description' type='text'>
-        //
-        //      <button type='submit'>Add task</button>
-        // </form>
-        // ";
-        //
-        // $output = $output . "
-        // <form action='/delete_tasks' method='post'>
-        //     <button type='submit'>delete</button>
-        // </form>
-        // ";
+
 
         //NOTE: give twig all the variables it needs
         return $silex_app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
-
-    $silex_app->post("/tasks", function() {
+        //NOTE: MAKE SURE THAT YOU INCLUDE THE USE ($silex_app), DAMNIT, IT IS EASY TO MISS THE SECOND TIME..
+    $silex_app->post("/tasks", function() use ($silex_app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+        return $silex_app['twig']->render('create_task.html.twig', array('new_task' => $task));
     });
 
     $silex_app->post('/delete_tasks', function(){
